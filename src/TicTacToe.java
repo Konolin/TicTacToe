67a98acf75e464362cf168362da82c60c43c2863
Konolin <mandru.catalin03@gmail.com>
2023-08-27 13:07:23 +0300

    Frame

---
+++ src/TicTacToe.java
@@ -0,0 +1,39 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class TicTacToe implements ActionListener {
+    Random random = new Random();
+    JFrame frame = new JFrame();
+    JPanel titlePanel = new JPanel();
+    JPanel buttonPanel = new JPanel();
+    JLabel textField = new JLabel();
+    JButton[] buttons = new JButton[9];
+    boolean player1Turn;
+
+    TicTacToe() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void firstTurn() {
+
+    }
+
+    public void check() {
+
+    }
+
+    public void xWins(int a, int b, int c) {
+
+    }
+
+    public void oWins(int a, int b, int c) {
+
+    }
+}
